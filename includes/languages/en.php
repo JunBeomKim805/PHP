e2686f7b82868f8839a25acf52d1c8e501175b1f
Junbeom Kim <jkim@launchfire.com>
2022-01-20 15:26:07 -0500

    .

---
+++ includes/languages/en.php
@@ -0,0 +1,8 @@
+<?php 
+
+const _REGISTER = "Register";
+const _USERNAME = "Enter your user name";
+const _EMAIL = "Enter your email";
+const _PASSWORD = "Enter your password";
+
+?>
--- registration.php
+++ registration.php
@@ -2,6 +2,21 @@
 <?php  include "includes/header.php"; ?>
 <?php 
 
+if(isset($_GET['lang']) && !empty($_GET['lang'])){
+    if (session_status() === PHP_SESSION_NONE) session_start();
+    $_SESSION['lang'] = $_GET['lang'];
+    
+    if(isset($_SESSION['lang']) && $_SESSION['lang'] != $_GET['lang']){
+        echo "<script type='text/javascript'>location.reload();</script>";
+    }
+}
+    
+    if(isset($_SESSION['lang'])){
+        include "includes/languages/".$_SESSION['lang'].".php";
+    }else{
+        include "includes/languages/en.php";
+    }
+
 
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
     $username = trim($_POST['user_name']); 
@@ -58,28 +73,38 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
     <!-- Page Content -->
     <div class="container">
     
+    <form method="get" class="navbar-form navbar-right" action=""id="language_form">
+        <div class="form-group">
+            <select class="form-control" onchange="changeLanguage()" name="lang" >
+                <option value="en" <?php if(isset($_SESSION['lang'])&& $_SESSION['lang']=='en'){echo "selected";} ?>>English</option>
+                <option value="es" <?php if(isset($_SESSION['lang'])&& $_SESSION['lang']=='es'){echo "selected";} ?>>Spanish</option>
+            </select>
+        </div>
+    </form>
+
+
 <section id="login">
     <div class="container">
         <div class="row">
             <div class="col-xs-6 col-xs-offset-3">
                 <div class="form-wrap">
-                <h1>Register</h1>
+                <h1><?php echo _REGISTER ?></h1>
                     <form role="form" action="registration.php" method="post" id="login-form" autocomplete="off">
                         
                         <div class="form-group">
                             <label for="user_name" class="sr-only">username</label>
-                            <input type="text" name="user_name" id="user_name"  value="<?php echo isset($username) ? $username : '' ?>" autocomplete="on" class="form-control" placeholder="Enter Desired Username">
+                            <input type="text" name="user_name" id="user_name"  value="<?php echo isset($username) ? $username : '' ?>" autocomplete="on" class="form-control" placeholder="<?php echo _USERNAME ?>">
                             <p><?php echo isset($error['user_name']) ? $error['user_name'] : '' ?></p>
                         </div>
                          <div class="form-group">
                             <label for="email" class="sr-only">Email</label>
-                            <input type="email" value="<?php echo isset($email) ? $email : '' ?>" autocomplete="on" name="email" id="email" class="form-control" placeholder="somebody@example.com">
+                            <input type="email" value="<?php echo isset($email) ? $email : '' ?>" autocomplete="on" name="email" id="email" class="form-control" placeholder="<?php echo _EMAIL ?>">
                             <p><?php echo isset($error['email']) ? $error['email'] : '' ?></p>
 
                         </div>
                          <div class="form-group">
                             <label for="password" class="sr-only">Password</label>
-                            <input type="password"  name="password" id="key" class="form-control" placeholder="Password">
+                            <input type="password"  name="password" id="key" class="form-control" placeholder="<?php echo _PASSWORD ?>">
                             <p><?php echo isset($error['password']) ? $error['password'] : '' ?></p>
 
                         </div>
@@ -96,6 +121,10 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 
         <hr>
 
-
+<script>
+    function changeLanguage(){
+        document.getElementById('language_form').submit();
+}
+</script>
 
 <?php include "includes/footer.php";?>
